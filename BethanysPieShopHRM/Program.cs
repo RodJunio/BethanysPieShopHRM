b28526fa29be6f8ca3d24ccf79574de3c22f6590
Junio Rodrigues <devjuniorodrigues@gmail.com>
2023-12-10 22:35:05 -0300

    ajustando para rider

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BethanysPieShopHRM.HR;
+using System.Text;
+
+
+IEmployee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+Console.WriteLine($"Hou many hours do we need to register for Bethany?");
+int numberOfHours = 0;
+string input = Console.ReadLine();
+numberOfHours = int.Parse(input);
+
+bethany.DisplayEmployeeDetails();
+bethany.PerformWork();
+bethany.PerformWork(numberOfHours);
